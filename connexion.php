372cb8c7dcf341eed17716647c977913c226a0cc
Zinabl <122523261+Zinabl@users.noreply.github.com>
2023-01-12 11:08:25 +0100

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -1,3 +1,3 @@
 <?php
-$link=mysqli_connect("localhost","root","","rat")or die("Echec de connexion à la base");
+$link=mysqli_connect("https://github.com/","Zinabl","ziineb 123","rat")or die("Echec de connexion à la base");
 ?>
